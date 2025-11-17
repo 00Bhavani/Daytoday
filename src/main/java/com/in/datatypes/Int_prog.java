94a03d447fddedaa31cffcd5c77236307a9b7897
00Bhavani <bhavanigowda0906@gmail.com>
2025-11-17 22:42:08 +0530

    int prog

--- src/main/java/com/in/datatypes/Int_prog.java
+++ src/main/java/com/in/datatypes/Int_prog.java
@@ -1,4 +1,23 @@
 package com.in.datatypes;
 
 public class Int_prog {
+    public static void main(String[] args) {
+        byte smallNumber = 100;
+        short mediumNumber = 30000;
+        int integerNumber = 100000;
+        long largeNumber = 10000000000L;  // Note 'L' suffix
+        float decimalNumber = 10.5f;      // Note 'f' suffix
+        double preciseDecimal = 20.99;
+        boolean isJavaFun = true;
+        char grade = 'A';
+
+        System.out.println("byte value: " + smallNumber);
+        System.out.println("short value: " + mediumNumber);
+        System.out.println("int value: " + integerNumber);
+        System.out.println("long value: " + largeNumber);
+        System.out.println("float value: " + decimalNumber);
+        System.out.println("double value: " + preciseDecimal);
+        System.out.println("boolean value: " + isJavaFun);
+        System.out.println("char value: " + grade);
+    }
 }
